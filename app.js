65da83ace67cf98122e9182c79ac464fd20f4e18
Yggy <igor.yentaltsev@gmail.com>
2013-11-17 13:36:26 -0500

    Added routes/index.js

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require('express'),
 	  app = express();
 var path = require('path');
+var routes  = require( "./routes" );
 
+// app.use ***********************************
 app.use(express.static(path.join(__dirname + "/public")));
+// app.use ***********************************
 
 
-app.get('/', function(req, res){
+// app.get ***********************************
+app.get('/hello', function(req, res){
   var body = 'Hello World';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
   res.end(body);
 });
+
+app.get("/", routes.pages("index"));
+// app.get ***********************************
 
 app.listen(1975);
 console.log('Listening on port 1975');
