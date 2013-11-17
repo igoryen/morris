c81403d44cd8bf0f41f96a8bec133c45c0903984
Yggy <igor.yentaltsev@gmail.com>
2013-11-17 12:56:23 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(1975);
+console.log('Listening on port 1975');
