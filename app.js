0f627280590cfc2fd2f7667df766b76753a02e10
Yggy <igor.yentaltsev@gmail.com>
2013-11-19 22:11:50 -0500

    Added image handling

--- app.js
+++ app.js
@@ -3,18 +3,40 @@ var express = require('express'), // 10
 	  app = express();
 var path = require('path'); // 20
 var nunjucks = require( "nunjucks" ); // 30
-nunjucks.configure('public', { // 31
-    autoescape: true, // 32 
-    express: app    // 33
-});
+var nunjucksEnv   = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, 'views' ))); // create the environment for the templating system
 var routes  = require( "./routes" ); // 40 
+var cwd_name = process.cwd(); // take process.cwd [current working dir] and assign ... to $dirname
 
-app.get('/hello', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+
+// ***** test 1 **********************************
+// nunjucks.configure( { autoescape: true } );
+// var str = nunjucks.renderString('Hi {{ username }}', { username: 'Yggy' });
+// console.log(str); // => in the shell: Hello Yggy
+
+
+// ***** test 2 *********************************
+// nunjucks.configure('views', { autoescape: true });
+// nunjucks.render('index.html', { author: 'igoryen' });
+
+
+// ***** test 3 *********************************
+// nunjucks.configure('views', {
+//    autoescape: true,
+//    express: app
+// });
+
+app.configure( function (){
+	nunjucksEnv.express( app );
+	app.use( express.static( path.join( __dirname  + "/public") ) ); 
+  app.use( express.static(cwd_name));  
 });
+
+
+// nunjucks.configure('views', { // 31
+//     express: app,    // 33
+//     autoescape: true // 32      
+// });
+
 
 app.get("/", routes.pages("index"));
 
